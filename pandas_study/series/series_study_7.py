186b6c2d8e92d07ee74fef14ca7a6d0e3e931ab2
LE-T <vincentjava@yahoo.com.tw>
2022-07-20 19:40:43 +0800

    p

---
+++ pandas_study/series/series_study_7.py
@@ -0,0 +1,9 @@
+import pandas as pd
+# Series 計算 3
+s = pd.Series([1, 2, -3, -4, 5], index=pd.date_range('20220701', periods=5))
+print(s)
+# 移動窗格 rolling(n)
+a = s.rolling(2).sum()
+print(a)
+b = s.rolling(2).sum().max()
+print(b)
