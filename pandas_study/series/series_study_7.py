e3dcb5ba75731d376bb61bce74971a399ffc2cd0
LE-T <vincentjava@yahoo.com.tw>
2022-07-20 19:47:10 +0800

    p

--- pandas_study/series/series_study_7.py
+++ pandas_study/series/series_study_7.py
@@ -1,6 +1,7 @@
 import pandas as pd
 # Series 計算 3
-s = pd.Series([1, 2, -3, -4, 5], index=pd.date_range('20220701', periods=5, freq='M'))
+s = pd.Series([1, 2, -3, -4, 5],
+              index=pd.date_range('20220701', periods=5, freq='D'))
 print(s)
 # 移動窗格 rolling(n)
 a = s.rolling(2).sum()
