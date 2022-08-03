0e6ea67755fd3a5ccc3f72e7a48b4d75977dd695
LE-T <vincentjava@yahoo.com.tw>
2022-08-03 21:04:30 +0800

    p

--- 每日股價/抓取每日股價.py
+++ 每日股價/抓取每日股價.py
@@ -14,6 +14,13 @@ for line in lines:
 #print(newlines)
 data = '\n'.join(newlines)
 df = pd.read_csv(io.StringIO(data))
+# 將全部資料變為 str
+df = df.astype(str)
+# 除去內容中有[,]的資料
+def func(s):
+    return s.str.replace(',', '')
+df = df.apply(func)
 print(df)
+
 
 
