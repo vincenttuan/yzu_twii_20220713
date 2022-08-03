909c256d6cdfd0a69f7381317c6d6fd80c2b6d88
LE-T <vincentjava@yahoo.com.tw>
2022-08-03 19:00:11 +0800

    p

--- pandas_study/dataframe/dataframe_study_2.py
+++ pandas_study/dataframe/dataframe_study_2.py
@@ -1,5 +1,6 @@
 # Serial 與 DataFrame
 import pandas as pd
+import matplotlib.pyplot as plt
 idx = pd.date_range('20220701', periods=5)
 # print(idx)
 s1 = pd.Series([1, 2, 3, 4, 5], index=idx)
@@ -8,4 +9,11 @@ s3 = pd.Series([6, 10, 2, 4, 6], index=idx)
 dict = {'s1': s1, 's2': s2, 's3': s3}
 df = pd.DataFrame(dict)
 print(df)
-print(df.loc['20220703'])
+# print(df.loc['20220703'])
+# 資料形狀
+print('形狀:', df.shape)
+print('列數:', df.shape[0])
+print('欄數:', df.shape[1])
+# 繪圖
+df.plot()
+plt.show()
