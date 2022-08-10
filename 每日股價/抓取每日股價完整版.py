aa903d03878c39d9c64061227aa981a65bc1664d
LE-T <vincentjava@yahoo.com.tw>
2022-08-10 19:29:57 +0800

    p

--- 每日股價/抓取每日股價完整版.py
+++ 每日股價/抓取每日股價完整版.py
@@ -18,7 +18,22 @@ def create_record(date):
     # [1, 2, 3], [4, 5, 6] => "1, 2, 3\n4, 5, 6"
     data = "\n".join(newlines)
     df = pd.read_csv(io.StringIO(data)) # 將字串data轉成csv檔案格式並給pd來讀取
-    print(df)
+    # 將全部資料變為 str
+    df = df.astype(str)
+    # 除去內容中有[,]的資料
+    df = df.apply(lambda s: s.str.replace(',', ''))
+    # 加入日期
+    df['交易日'] = date
+    # 設定 交易日 與 證券代號為 index
+    df = df.set_index(['交易日', '證券代號'])
+    # 去除非數字的資料 (coerce 表示若轉換失敗就賦予 NaN)
+    df = df.apply(lambda s: pd.to_numeric(s, errors='coerce'))
+    # 將欄位有 NaN的都要移除
+    df = df.dropna(axis=1, how='all')
+    # 先將 df 存成 csv 再轉存到 sqlite
+    df.to_csv("price.csv", encoding="utf-8")  # utf-8_sig
+    conn = sqlite3.connect('../資料庫/財經資料庫.db')
+    df.to_sql('price', conn, if_exists='append')
 
 if __name__ == '__main__':
     today = '2022-08-10'
