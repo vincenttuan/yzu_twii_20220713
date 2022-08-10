26d1873fe01cdad5609d25d5b2ca0a7d26085d07
LE-T <vincentjava@yahoo.com.tw>
2022-08-10 20:40:27 +0800

    p

---
+++ 繪製股價走勢圖/走勢圖.py
@@ -0,0 +1,24 @@
+'''
+https://pypi.org/project/mplfinance/
+安裝 pip install --upgrade mplfinance
+'''
+import sqlite3
+import pandas as pd
+import mplfinance as mpf
+
+if __name__ == '__main__':
+    # 資料庫路徑
+    db_path = '../資料庫/財經資料庫.db'
+    conn = sqlite3.connect(db_path)
+    # 查找某一檔股票最近n天的股價資料
+    symbol = '0050'
+    amount = '20'
+    sql = '''
+        select 交易日 as Date, 開盤價 as Open, 最高價 as 'High',
+               最低價 as Low, 收盤價 as Close, 成交金額 as Volume
+        from price
+        where 證券代號 = '%s' order by 交易日 desc limit %s        
+    ''' % (symbol, amount)
+    print(sql)
+
+
