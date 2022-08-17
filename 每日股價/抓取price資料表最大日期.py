6d750f009d2bf1ed06d78047c1f4522a5c12d1e3
LE-T <vincentjava@yahoo.com.tw>
2022-08-17 18:36:54 +0800

    p

--- 每日股價/抓取price資料表最大日期.py
+++ 每日股價/抓取price資料表最大日期.py
@@ -7,7 +7,10 @@ cursor = conn.cursor()
 sql = 'select max(交易日) from price'
 cursor.execute(sql)
 row = cursor.fetchone();
+print(row[0], type(row[0]))
+# strptime 字串轉時間日期, .date() 轉日期
 max_date = datetime.strptime(row[0], '%Y-%m-%d').date()
-print('報價資料表最大日期:', max_date)
+print('報價資料表最大日期:', max_date, type(max_date))
+# timedelta(days=1) 加一天
 max_date_add_one_day = max_date + timedelta(days=1)
 print('報價資料表開始抓取日期:', max_date_add_one_day)
