d879d64e6ee68197426e01cf69df0f493f2b883c
LE-T <vincentjava@yahoo.com.tw>
2022-08-17 20:30:26 +0800

    p

--- 每日股價/抓取price資料表最大日期.py
+++ 每日股價/抓取price資料表最大日期.py
@@ -2,12 +2,17 @@ import sqlite3
 from datetime import datetime, date, timedelta
 
 def get_begin_day():
+    # 資料庫連線
     conn = sqlite3.connect('../資料庫/財經資料庫.db')
+    # 資料指標
     cursor = conn.cursor()
-
     sql = 'select max(交易日) from price'
+    # 透過資料指標去執行 sql
     cursor.execute(sql)
+    # fetchone 得到一筆資料
     row = cursor.fetchone();
+    print(row, type(row))
+    # row[0] 第一筆資料
     print(row[0], type(row[0]))
     # strptime 字串轉時間日期, .date() 轉日期
     max_date = datetime.strptime(row[0], '%Y-%m-%d').date()
@@ -16,3 +21,6 @@ def get_begin_day():
     max_date_add_one_day = max_date + timedelta(days=1)
     print('報價資料表開始抓取日期:', max_date_add_one_day)
     return max_date_add_one_day
+
+if __name__ == '__main__':
+    print(get_begin_day())
