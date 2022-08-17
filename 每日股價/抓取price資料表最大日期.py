abaf0ab5828e22cf052afd6b536db168092809f1
LE-T <vincentjava@yahoo.com.tw>
2022-08-17 18:31:02 +0800

    p

---
+++ 每日股價/抓取price資料表最大日期.py
@@ -0,0 +1,13 @@
+import sqlite3
+from datetime import datetime, date, timedelta
+
+conn = sqlite3.connect('../資料庫/財經資料庫.db')
+cursor = conn.cursor()
+
+sql = 'select max(交易日) from price'
+cursor.execute(sql)
+row = cursor.fetchone();
+max_date = datetime.strptime(row[0], '%Y-%m-%d').date()
+print('報價資料表最大日期:', max_date)
+max_date = max_date + timedelta(days=1)
+print('報價資料表開始抓取日期:', max_date)
