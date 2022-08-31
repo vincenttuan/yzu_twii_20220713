da9e621e53346935c0bd534de3c352e3694b1b8d
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-08-31 19:27:13 +0800

    Add files via upload

--- 一籃子股票/股票張數分配.py
+++ 一籃子股票/股票張數分配.py
@@ -22,6 +22,22 @@ def portfolio(stock_list, money):
     stock_list = price.iloc[0][stock_list]
     print(stock_list)
 
+    while True:
+        # 平均每一檔股票可以有多少錢買進?
+        invest_money = (money / len(stock_list))
+        print('每一檔股票可以買入的金額:', invest_money)
+        # np.floor 往下取整數 1.2 -> 1, -1.2 -> -2
+        ret = np.floor(invest_money / stock_list / 1000)
+        print(ret)
+        if (ret == 0).any(): # 假設 ret 列表中有 0.0 的情況
+            # 若沒有匹配到張數的就踢除掉價格最大的標的
+            print("剔除標的:", str(stock_list.idxmax()), "價格:", str(stock_list.max()))
+            stock_list = stock_list[stock_list != stock_list.max()]
+            # print(stock_list)
+        else:
+            break;
+
+
 if __name__ == '__main__':
     # 選擇一個有交易的日期當作買入日期
     tday = datetime.date(2022, 7, 1)
@@ -29,6 +45,6 @@ if __name__ == '__main__':
     cond, index = basket(tday)
     print('cond:', cond)
     # 投資金額
-    money = 300000
+    money = 2000000
     # 建議投資分配
     portfolio(cond, money)
